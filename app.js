eade8454a70b0f82bf2be12c41477a5b613cd777
Kevin Castro <kevin.castro.chavez0@gmail.com>
2022-05-02 15:37:02 -0600

    learned how to parse request bodies

--- app.js
+++ app.js
@@ -17,12 +17,25 @@ const server = http.createServer((req, res) => {
   }
 
   if (url === '/message' && method === 'POST') {
-    fs.writeFileSync('message.txt', 'DUMMY');
+    const body = [];
+    req.on('data', (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+
+    req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      console.log(parsedBody);
+      fs.writeFileSync('message.txt', message);
+    });
+
     res.statusCode = 302;
     res.setHeader('Location', '/');
 
     return res.end();
   }
+
   res.write('<html>');
   res.write('<head><title>Hi</title></head>');
   res.write('<body><p>Hi</p></body>');
