81d91f2d4ba1cd6459641355615019ffbdaee667
Kevin Castro <kevin.castro.chavez0@gmail.com>
2022-05-02 11:23:43 -0600

    learned how to redirect requests, send status codes and write files on certain conditions

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
   const url = req.url;
+  const method = req.method;
+
   if (url === '/') {
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
@@ -13,6 +16,13 @@ const server = http.createServer((req, res) => {
     return res.end();
   }
 
+  if (url === '/message' && method === 'POST') {
+    fs.writeFileSync('message.txt', 'DUMMY');
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
+
+    return res.end();
+  }
   res.write('<html>');
   res.write('<head><title>Hi</title></head>');
   res.write('<body><p>Hi</p></body>');
