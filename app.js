61e5602dccf336f77539f59168cbd72761bbd0dc
Kevin Castro <kevin.castro.chavez0@gmail.com>
2022-05-02 11:10:05 -0600

    learned about sending responses and writing code

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method, req.headers);
   //   process.exit();
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>Hi</title></head>');
+  res.write('<body><p>Hello</p></body>');
+  res.write('</html>');
+  res.end();
 });
 
 server.listen(3000);
