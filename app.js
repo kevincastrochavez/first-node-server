9edbbd40c8b897fed146d22c36f451c90069532b
Kevin Castro <kevin.castro.chavez0@gmail.com>
2022-05-02 11:18:33 -0600

    learned about routing requests and how to conditionally show content

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
-  //   process.exit();
-  res.setHeader('Content-Type', 'text/html');
+  const url = req.url;
+  if (url === '/') {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Hi</title></head>');
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+    );
+    res.write('</html>');
+    return res.end();
+  }
+
   res.write('<html>');
   res.write('<head><title>Hi</title></head>');
-  res.write('<body><p>Hello</p></body>');
+  res.write('<body><p>Hi</p></body>');
   res.write('</html>');
-  res.end();
 });
 
 server.listen(3000);
