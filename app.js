c7541cb3b5acca539a64761567e74532e60e6c70
Kevin <kevin.castro.chavez0@gmail.com>
2022-05-30 13:34:15 -0600

    created npm script

--- app.js
+++ app.js
@@ -1,44 +1,9 @@
 const http = require('http');
-const fs = require('fs');
+
+const routes = require('./routes');
 
 const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-
-  if (url === '/') {
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>Hi</title></head>');
-    res.write(
-      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
-    );
-    res.write('</html>');
-    return res.end();
-  }
-
-  if (url === '/message' && method === 'POST') {
-    const body = [];
-    req.on('data', (chunk) => {
-      console.log(chunk);
-      body.push(chunk);
-    });
-
-    return req.on('end', () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split('=')[1];
-
-      fs.writeFileSync('message.txt', message);
-      res.statusCode = 302;
-      res.setHeader('Location', '/');
-
-      return res.end();
-    });
-  }
-
-  res.write('<html>');
-  res.write('<head><title>Hi</title></head>');
-  res.write('<body><p>Hi</p></body>');
-  res.write('</html>');
+  routes;
 });
 
 server.listen(3000);
