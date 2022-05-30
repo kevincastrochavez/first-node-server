2995ab7db3093d6dea9d624aab86db8725beb099
Kevin <kevin.castro.chavez0@gmail.com>
2022-05-30 14:09:48 -0600

    learned how middleware works

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log('In the middleware');
+  next(); // Allows request to continue to next middleware in line. Otherwise, it won't run
+});
+
+app.use((req, res, next) => {
+  console.log('In another middleware');
+});
+
 const server = http.createServer(app);
 
 server.listen(3000);
