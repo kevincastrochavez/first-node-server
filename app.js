a2a494fb736d53713921cce86cb3e8799d298264
Kevin <kevin.castro.chavez0@gmail.com>
2022-05-31 22:11:55 -0600

    handled different routes

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('In the middleware');
-  next(); // Allows request to continue to next middleware in line. Otherwise, it won't run
+app.use('/', (req, res, next) => {
+  console.log('This always runs');
+  next();
 });
 
-app.use((req, res, next) => {
+app.use('/hello', (req, res, next) => {
+  console.log('In another middleware');
+  res.send('<h1>Second</h1>'); // Sending a response is another alternative
+});
+
+app.use('/', (req, res, next) => {
   console.log('In another middleware');
   res.send('<h1>Hello</h1>'); // Sending a response is another alternative
 });
