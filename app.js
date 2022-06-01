1811fe48b48bd8571814150a3459bf7a39105a48
Kevin <kevin.castro.chavez0@gmail.com>
2022-05-31 22:39:45 -0600

    parsed incoming requests with body-parse

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+// Parses the response for forms
+app.use(bodyParser.urlencoded());
+
 app.use('/', (req, res, next) => {
-  console.log('This always runs');
   next();
 });
 
-app.use('/hello', (req, res, next) => {
-  console.log('In another middleware');
-  res.send('<h1>Second</h1>'); // Sending a response is another alternative
+app.use('/add-product', (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>'
+  ); // Sending a response is another alternative
+});
+
+app.use('/product', (req, res, next) => {
+  console.log(req.body.title);
+  res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {
-  console.log('In another middleware');
   res.send('<h1>Hello</h1>'); // Sending a response is another alternative
 });
 
